adea322d2ace9d44a7a0dc75985d807081e525b6
Tomasz Majer <17414256+tpmajer@users.noreply.github.com>
2026-04-21 09:09:25 +0200

    feat(fish): auto-commit flake.lock changes after nix flake update

--- .config/fish/functions/nix.fish
+++ .config/fish/functions/nix.fish
@@ -1,4 +1,16 @@
-function nix --wraps=nix --description 'alias nix=systemd-inhibit nix'
-  systemd-inhibit nix $argv
-        
+function nix --wraps=nix --description 'wrapper for nix: runs via systemd-inhibit and auto-commits flake.lock changes'
+    set flake_before (md5sum ~/.nixos/flake.lock)
+
+    systemd-inhibit nix $argv
+    set exit_code $status
+
+    if test $exit_code -eq 0
+        set flake_after (md5sum ~/.nixos/flake.lock)
+        if test "$flake_before" != "$flake_after"
+            git -C ~/.nixos add flake.lock
+            git -C ~/.nixos commit -m "chore: update flake.lock"
+        end
+    end
+
+    return $exit_code
 end
