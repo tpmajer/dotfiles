80ea85418c3e2c59089cc10e63db6636da637639
Tomasz Majer <17414256+tpmajer@users.noreply.github.com>
2026-04-21 09:38:07 +0200

    fix(fish): commit flake.lock based on git state instead of md5sum comparison

--- .config/fish/functions/nh.fish
+++ .config/fish/functions/nh.fish
@@ -1,12 +1,9 @@
 function nh --wraps=nh --description 'wrapper for nh: runs via systemd-inhibit and auto-commits flake.lock changes'
-    set flake_before (md5sum ~/.nixos/flake.lock)
-
     systemd-inhibit nh $argv
     set exit_code $status
 
     if test $exit_code -eq 0
-        set flake_after (md5sum ~/.nixos/flake.lock)
-        if test "$flake_before" != "$flake_after"
+        if not git -C ~/.nixos diff --quiet flake.lock
             git -C ~/.nixos add flake.lock
             git -C ~/.nixos commit -m "chore: update flake.lock"
         end
--- .config/fish/functions/nix.fish
+++ .config/fish/functions/nix.fish
@@ -1,12 +1,9 @@
 function nix --wraps=nix --description 'wrapper for nix: runs via systemd-inhibit and auto-commits flake.lock changes'
-    set flake_before (md5sum ~/.nixos/flake.lock)
-
     systemd-inhibit nix $argv
     set exit_code $status
 
     if test $exit_code -eq 0
-        set flake_after (md5sum ~/.nixos/flake.lock)
-        if test "$flake_before" != "$flake_after"
+        if not git -C ~/.nixos diff --quiet flake.lock
             git -C ~/.nixos add flake.lock
             git -C ~/.nixos commit -m "chore: update flake.lock"
         end
