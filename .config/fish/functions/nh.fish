b822d3f9bf71b248c531b14113687af83482d43d
Tomasz Majer <17414256+tpmajer@users.noreply.github.com>
2026-04-21 09:04:19 +0200

    feat(fish): auto-commit flake.lock changes after nh os switch/boot -u

--- .config/fish/functions/nh.fish
+++ .config/fish/functions/nh.fish
@@ -1,3 +1,16 @@
-function nh --wraps=nh --description 'alias nh=systemd-inhibit nh'
+function nh --wraps=nh --description 'wrapper for nh: runs via systemd-inhibit and auto-commits flake.lock changes'
+    set flake_before (md5sum ~/.nixos/flake.lock)
+
     systemd-inhibit nh $argv
+    set exit_code $status
+
+    if test $exit_code -eq 0
+        set flake_after (md5sum ~/.nixos/flake.lock)
+        if test "$flake_before" != "$flake_after"
+            git -C ~/.nixos add flake.lock
+            git -C ~/.nixos commit -m "chore: update flake.lock"
+        end
+    end
+
+    return $exit_code
 end
